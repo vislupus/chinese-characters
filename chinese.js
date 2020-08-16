d1d58bff6eabd6d6608bfd4991f7d9240644a4a8
Nikola Bozhinov <nikolaobozhinov@gmail.com>
2020-08-16 12:26:49 +0300

    show score

--- chinese.js
+++ chinese.js
@@ -2,220 +2,308 @@ let charactersData = [{
     id: 1,
     character: "男孩",
     english: "boy",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 2,
     character: "女孩",
     english: "girl",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 3,
     character: "女人",
     english: "woman",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 4,
     character: "男人",
     english: "man",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 5,
     character: "女性",
     english: "female",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 6,
     character: "男性",
     english: "male",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 7,
     character: "儿子",
     english: "son",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 8,
     character: "女儿",
     english: "daughter",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 9,
     character: "孩子",
     english: "child",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 10,
     character: "母亲",
     english: "mother",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 11,
     character: "父亲",
     english: "father",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 12,
     character: "妹妹",
     english: "sister",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 13,
     character: "哥哥",
     english: "brother",
-    group: "family"
+    group: "family",
+    correct:0,
+    wrong:0
 }, {
     id: 14,
     character: "红色",
     english: "red",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 15,
     character: "绿色",
     english: "green",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 16,
     character: "蓝色",
     english: "blue",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 17,
     character: "黄色",
     english: "yellow",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 18,
     character: "黑色",
     english: "black",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 19,
     character: "白色",
     english: "white",
-    group: "color"
+    group: "color",
+    correct:0,
+    wrong:0
 }, {
     id: 20,
     character: "一",
     english: "one",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 21,
     character: "二",
     english: "two",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 22,
     character: "三",
     english: "three",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 23,
     character: "四",
     english: "four",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 24,
     character: "五",
     english: "five",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 25,
     character: "六",
     english: "six",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 26,
     character: "七",
     english: "seven",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 27,
     character: "八",
     english: "eight",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 28,
     character: "九",
     english: "nine",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 29,
     character: "十",
     english: "ten",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 30,
     character: "百",
     english: "hundred",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 31,
     character: "我",
     english: "I",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 32,
     character: "你",
     english: "you",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 33,
     character: "名字",
     english: "first name",
-    group: "greeting"
+    group: "greeting",
+    correct:0,
+    wrong:0
 }, {
     id: 34,
     character: "姓",
     english: "last name",
-    group: "greeting"
+    group: "greeting",
+    correct:0,
+    wrong:0
 }, {
     id: 35,
     character: "再见",
     english: "bye",
-    group: "greeting"
+    group: "greeting",
+    correct:0,
+    wrong:0
 }, {
     id: 36,
     character: "你好",
     english: "hi",
-    group: "greeting"
+    group: "greeting",
+    correct:0,
+    wrong:0
 }, {
     id: 37,
     character: "零",
     english: "zero",
-    group: "number"
+    group: "number",
+    correct:0,
+    wrong:0
 }, {
     id: 38,
     character: "他",
     english: "he",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 39,
     character: "她",
     english: "she",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 40,
     character: "它",
     english: "it",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 41,
     character: "我们",
     english: "we",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 42,
     character: "他们",
     english: "they",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 43,
     character: "是",
     english: "to be",
-    group: "pronoun"
+    group: "pronoun",
+    correct:0,
+    wrong:0
 }, {
     id: 44,
     character: "猫",
     english: "cat",
-    group: "animal"
+    group: "animal",
+    correct:0,
+    wrong:0
 }];
